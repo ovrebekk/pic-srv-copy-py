65109eeff0af7c386aa405380bece7ba63ad71b2
Torbjørn Øvrebekk <ovrebekk@gmail.com>
2024-01-17 23:47:04 +0100

    Weekday removed from %DAY

--- main.py
+++ main.py
@@ -186,8 +186,8 @@ class CmdFile:
             outFile.write(CmdFileKeywords.TARGET_COPY_FOLDER + cmdFileSep + self.targetFolderOnLastCopy + '\n')
 
     def getDayString(self):
-        dayString = datetime.strftime(self.dtCreationDate, "%a_")
-        dayString += str(self.dtCreationDate.day)
+        #dayString = datetime.strftime(self.dtCreationDate, "%a_")
+        dayString = str(self.dtCreationDate.day)
         if self.dtCreationDate.day >= 10 and self.dtCreationDate.day <= 20:
             dayString += dayPostFix[9]
         else:
